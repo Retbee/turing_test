fd93226efedf30ff6f508199436ceb169f2b148d
Retbee <forengbee@gmail.com>
2021-08-21 20:40:51 +0300

    Commit to new repository

---
+++ src/homework/week5Day3Part1/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const week5Day3Part1 = () => {
+    return <p>undone homework week 5 day 3 part 1</p>
+}
+
+export default week5Day3Part1
---
+++ src/homework/week5Day3Part2/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const week5Day3Part2 = () => {
+    return <p>undone homework week 5 day 3 part 2</p>
+}
+
+export default week5Day3Part2
