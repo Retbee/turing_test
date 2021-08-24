5ec80222e40a9677543eff797ff668d9117e2961
Retbee <forengbee@gmail.com>
2021-08-24 20:13:19 +0300

    1 and  2 task w5d3p1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
 import React from "react";
-import week5Day3Part1 from "./homework/week5Day3Part1";
+import Week5Day3Part1 from "./homework/week5Day3Part1";
 import week5Day3Part2 from "./homework/week5Day3Part2";
 import ListClass from "./todo";
+
+class App extends React.Component {
+    render () {
+        return (
+            <>
+                {/*<List/>*/}
+                <Week5Day3Part1/>
+                {/*<week5Day3Part2/>*/}
+                {/*<ListClass/>*/}
+            </>
+        )
+    }
+}
+
+export default App
--- src/homework/week5Day3Part1/index.js
+++ src/homework/week5Day3Part1/index.js
@@ -1,7 +1,94 @@
 import React from "react";
 
-const week5Day3Part1 = () => {
-    return <p>undone homework week 5 day 3 part 1</p>
+
+const Week5Day3Part1 = () => {
+    //1
+
+
+    const arrayToDisplay = [
+        {id: 1, value: 'First item'},
+        {id: 2, value: 'Second item'},
+        {id: 3, value: 'Third item'},
+    ]
+
+
+    let Task = arrayToDisplay.map((elem) => {
+        return (
+            <li>
+                <span>{elem.value}</span>
+                <button onClick={() => alert(elem.value)}>Press to alert text</button>
+            </li>
+        )
+    })
+
+
+
+
+
+    // 2 add to start of array
+
+
+    const [dataAddToStart, setDataAddToStart] = React.useState(
+        []
+    )
+
+    const handlePromptAddToStart = () => {
+        return (
+            setDataAddToStart([{id: dataAddToStart.length + 1, value: `${prompt()}`}, ...dataAddToStart])
+        )
+    }
+
+    const listDataToStart = dataAddToStart.map((data) => <li key={data.id}>{data.value}</li>)
+
+
+
+
+
+    //2 add to end of array
+
+
+    const [dataAddToEnd, setDataAddToEnd] = React.useState (
+        []
+    )
+
+    const ListDataToEnd = dataAddToEnd.map((elem) => <li key={elem.id}>{elem.value}</li>)
+
+    const handlePromptAddToEnd = () => {
+            setDataAddToEnd(...dataAddToEnd, [{id: dataAddToEnd.length + 1, value: `${prompt()}`}]
+            
+        )
+
+    }
+
+
+
+    return (
+        <>
+            <ul>
+                {Task}
+            </ul>
+
+            <br/>
+            <br/>
+            <br/>
+
+            <ul>
+                {listDataToStart}
+            </ul>
+            <button onClick={handlePromptAddToStart}>Click to alert prompt and add data on star of array</button>
+
+
+            <br/>
+            <br/>
+            <br/>
+
+            <ul>
+                {ListDataToEnd}
+            </ul>
+            <button onClick={handlePromptAddToEnd}>Click to alert prompt and add data on end of array</button>
+        </>
+
+    )
 }
 
-export default week5Day3Part1
+export default Week5Day3Part1
